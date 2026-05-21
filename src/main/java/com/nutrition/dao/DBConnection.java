01aabaa87503a85ccce583400f71ac8d4b10de3b
Anant Joshi <joshiianant2@gmail.com>
2026-05-21 10:30:31 +0530

    Fix DBConnection: disable SSL for Railway internal network

--- src/main/java/com/nutrition/dao/DBConnection.java
+++ src/main/java/com/nutrition/dao/DBConnection.java
@@ -18,23 +18,27 @@ public class DBConnection {
         try {
             Class.forName("org.postgresql.Driver");
 
-            // Parse the URI to extract components
             URI uri = new URI(dbUrl);
             String host = uri.getHost();
             int port = uri.getPort() == -1 ? 5432 : uri.getPort();
-            String path = uri.getPath(); // e.g. /nutritiontracker_db
+            String path = uri.getPath();
             String[] userInfo = uri.getUserInfo().split(":");
             String user = userInfo[0];
             String password = userInfo[1];
 
-            String jdbcUrl = "jdbc:postgresql://" + host + ":" + port + path
-                           + "?sslmode=require";
+            String jdbcUrl = "jdbc:postgresql://" + host + ":" + port + path;
 
             Properties props = new Properties();
             props.setProperty("user", user);
             props.setProperty("password", password);
-            props.setProperty("sslmode", "require");
-            props.setProperty("sslfactory", "org.postgresql.ssl.NonValidatingFactory");
+
+            // Use SSL only for Render (not Railway internal network)
+            if (!host.contains("railway.internal")) {
+                props.setProperty("sslmode", "require");
+                props.setProperty("sslfactory", "org.postgresql.ssl.NonValidatingFactory");
+            } else {
+                props.setProperty("sslmode", "disable");
+            }
 
             return DriverManager.getConnection(jdbcUrl, props);
 
